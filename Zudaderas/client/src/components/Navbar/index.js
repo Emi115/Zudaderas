ba38497dbc000f9c6cdaf0b978892d30f303e84a
Emi115 <emiomg8@gmail.com>
2024-03-05 00:47:37 +0100

    Css actualizado
    Css actualizado para prevenir el cancer visual de pagina

--- Zudaderas/client/src/components/Navbar/index.js
+++ Zudaderas/client/src/components/Navbar/index.js
@@ -4,28 +4,36 @@ import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
-    // Supongamos que guardaste el rol del usuario en el localStorage
     const userRole = localStorage.getItem("userRole");
 
     return (
         <nav className={styles.navbar}>
-            <Link to="/" className={styles.navLink}>
-                Inicio
-            </Link>
-            <Link to="/about" className={styles.navLink}>
-                Sobre Nosotros
-            </Link>
-            {userRole === "admin" && (
-                <Link to="/crearsudaderas" className={styles.navLink}>
-                    Crear Sudaderas
+            <div className={styles.linksContainer}>
+                <Link to="/" className={styles.navLink}>
+                    Inicio
                 </Link>
-            )}
-            <Link to="/Formulario" className={styles.navLink}>
-                Login/Register
-            </Link>
-            <Link to="/" className={styles.navLink}>
-                Categorias
-            </Link>
+                <Link to="/about" className={styles.navLink}>
+                    Sobre Nosotros
+                </Link>
+                {userRole === "admin" && (
+                    <Link to="/crearsudaderas" className={styles.navLink}>
+                        Crear Sudaderas
+                    </Link>
+                )}
+            </div>
+            <img
+                src="/images/GladiusNofondo.png"
+                alt="Logo"
+                className={styles.logo}
+            />
+            <div className={styles.linksContainer}>
+                <Link to="/Formulario" className={styles.navLink}>
+                    Login/Register
+                </Link>
+                <Link to="/" className={styles.navLink}>
+                    Categorias
+                </Link>
+            </div>
         </nav>
     );
 }
