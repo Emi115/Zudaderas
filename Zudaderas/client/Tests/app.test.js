3832087aa2cead2d70a751909549d80cc5ddcba9
Emi115 <emiomg8@gmail.com>
2024-03-11 19:49:20 +0100

    Interrogacion puesta

--- Zudaderas/client/Tests/app.test.js
+++ Zudaderas/client/Tests/app.test.js
@@ -1,11 +1,11 @@
 // Importa el archivo app.js
 import app from '../src/components/App.js';
 
-// Describe el grupo de pruebas
+//! Describe el grupo de pruebas
 describe('App', () => {
-  // Prueba si el archivo app.js se carga correctamente
+  //! Prueba si el archivo app.js se carga correctamente
   it('should load app.js', () => {
-    // Verifica si app.js se ha cargado correctamente
+    //! Verifica si app.js se ha cargado correctamente
     expect(app).toBeDefined();
   });
 });
